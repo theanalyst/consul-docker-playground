9bfd3cac7192cb6ec9e7d552b33fd1a083ad5e16
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-02-12 19:39:57 +0530

    use port 8500 also rm container after exit

--- registrator.sh
+++ registrator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Consul container must be already running
-docker run -d -h $HOSTNAME -v /var/run/docker.sock:/tmp/docker.sock progrium/registrator consul://$(dig -p 8600 consul.service.consul +short)
+docker run --rm -h $HOSTNAME -v /var/run/docker.sock:/tmp/docker.sock progrium/registrator consul://$(dig -p 8600 consul.service.consul +short):8500
