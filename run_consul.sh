6a0b18ec840e11ab5f4eaa9d2d5f685ae067eafd
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-02-17 17:33:13 +0530

    run a named consul job

--- run_consul.sh
+++ run_consul.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v ~/spells/docker/consul:/data -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h node1 progrium/consul -server -bootstrap -ui-dir /ui
+docker run -d -v ~/spells/docker/consul:/data -p 8400:8400 -p 8301:8301 -p 8301:8301/udp -p 8300:8300 -p 8302:8302 -p 8302:8302/udp -p 8500:8500 -p 8600:53/udp -p 172.17.42.1:53:53/udp -h node1 --name consul progrium/consul -server -bootstrap -ui-dir /ui -advertise 192.168.122.1
